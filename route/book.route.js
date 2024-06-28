872112daee17a97a76a670113fde0d849ac918ac
Shikhar9960 <shikharjais5@gmail.com>
2024-06-28 14:22:01 +0530

    file changes

--- route/book.route.js
+++ route/book.route.js
@@ -2,6 +2,6 @@ import express from "express";
 import { getBook } from "../controller/book.controller.js";
 
 const router = express.Router();
-router.get("/", getBook);
+router.get("https://bookstore-backend-seven-chi.vercel.app/", getBook);
 
 export default router
