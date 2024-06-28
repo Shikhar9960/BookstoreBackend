97ea7241e76497158202ea05e8dfbac557c7cc76
Shikhar9960 <shikharjais5@gmail.com>
2024-06-28 14:34:51 +0530

    file changes

--- route/user.route.js
+++ route/user.route.js
@@ -2,7 +2,7 @@ import express from "express";
 import { login, signup } from "../controller/user.controller.js";
 const router =express.Router();
 
-router.post("https://bookstore-backend-seven-chi.vercel.app/signup",signup);
-router.post("https://bookstore-backend-seven-chi.vercel.app/login",login)
+router.post("/signup",signup);
+router.post("/login",login)
 
 export default router;
